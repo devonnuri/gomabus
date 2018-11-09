01c6690b0473bd0960cc5f008c9ffda5ef1ef20f
devonnuri <devonnuri@gmail.com>
2018-11-09 20:57:54 +0900

    Add BusStopScreen

--- screens/BusStopScreen.js
+++ screens/BusStopScreen.js
@@ -1,16 +1,87 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import {
+  TouchableOpacity,
+  ScrollView,
+  StyleSheet,
+  View,
+  Text,
+  TextInput
+} from 'react-native';
+import { Card, CardTitle, CardContent } from 'react-native-cards';
+import axios from 'axios';
 
 export default class BusStopScreen extends React.Component {
   static navigationOptions = {
     header: null
   };
 
+  state = {
+    searchStop: '',
+    searchResult: []
+  };
+
+  _onSearchChange = text => {
+    if (!text) {
+      this.setState({ ...this.state, searchResult: [] });
+      return;
+    }
+
+    let data = new FormData();
+    data.append('busStop', text);
+
+    axios({
+      method: 'post',
+      url: 'http://bis.gongju.go.kr/inq/searchBusStop.do',
+      data,
+      config: { headers: { 'Content-Type': 'multipart/form-data' } }
+    }).then(response => {
+      this.setState({ ...this.state, searchResult: response.data.busStopList });
+    });
+    this.setState({ ...this.state, searchStop: text });
+  };
+
   render() {
     return (
-      <ScrollView style={styles.container}>
-        <Text>Blah Blah</Text>
-      </ScrollView>
+      <View style={styles.container}>
+        <View style={styles.searchSection}>
+          <View style={styles.searchBar}>
+            <Text style={styles.searchLabel}>정류장 이름</Text>
+            <TextInput
+              style={styles.searchInput}
+              onChangeText={this._onSearchChange}
+              value={this.state.searchStop}
+              selectionColor="#fff"
+              underlineColorAndroid="transparent"
+            />
+          </View>
+        </View>
+        <View style={styles.resultSection}>
+          {this.state.searchResult.length > 0 ? (
+            <ScrollView>
+              {this.state.searchResult.map((stop, index) => (
+                <TouchableOpacity
+                  key={index}
+                  // onPress={() =>
+                  //   this.props.navigation.navigate('RouteDetail', {
+                  //     route
+                  //   })
+                  // }
+                >
+                  <Card>
+                    <CardTitle title={stop.stop_name} />
+                  </Card>
+                </TouchableOpacity>
+              ))}
+            </ScrollView>
+          ) : (
+            <View style={styles.noSearch}>
+              <Text style={styles.noSearchText}>
+                위 입력창에 원하시는 정류장 이름을 입력하세요.
+              </Text>
+            </View>
+          )}
+        </View>
+      </View>
     );
   }
 }
@@ -18,7 +89,64 @@ export default class BusStopScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
     backgroundColor: '#fff'
+  },
+  searchSection: {
+    padding: 10,
+    backgroundColor: 'rgb(195, 91, 39)'
+  },
+  searchBar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    borderRadius: 10
+  },
+  searchLabel: {
+    color: 'rgba(255, 255, 255, 0.7)',
+    paddingHorizontal: 5
+  },
+  searchInput: {
+    flexGrow: 2,
+    height: 40,
+    fontSize: 15,
+    color: '#fff'
+  },
+  resultSection: {
+    flex: 1,
+    backgroundColor: '#f0f0f0',
+    padding: 10
+  },
+  noSearch: {
+    flex: 1,
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  routeHeader: {
+    flex: 1,
+    flexDirection: 'row',
+    marginBottom: 8
+  },
+  iconRoute: {
+    marginRight: 5
+  },
+  iconBus: {
+    marginRight: 7
+  },
+  routeName: {
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  routeDirection: {
+    fontSize: 15,
+    marginLeft: 'auto'
+  },
+  routeExplain: {
+    flexDirection: 'row'
+  },
+  routeStop: {
+    flexDirection: 'row',
+    marginLeft: 3
   }
 });
--- screens/RouteDetailScreen.js
+++ screens/RouteDetailScreen.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import { BackHandler, View, ScrollView, StyleSheet, Text } from 'react-native';
+import {
+  Platform,
+  BackHandler,
+  View,
+  ScrollView,
+  StyleSheet,
+  Text
+} from 'react-native';
 import { Card, CardTitle, CardContent } from 'react-native-cards';
+import { Icon } from 'expo';
 import axios from 'axios';
 import FormData from 'form-data';
 
@@ -38,7 +46,7 @@ export default class RouteDetailScreen extends React.Component {
       .then(([{ data: stops }, { data: buses }]) => {
         this.setState({
           stops: stops.busRouteDetailList,
-          buses
+          buses: buses.busRealLocList
         });
       })
       .catch(() => {});
@@ -54,6 +62,7 @@ export default class RouteDetailScreen extends React.Component {
 
   render() {
     const { route } = this.props.navigation.state.params;
+    const stopsInBus = this.state.buses.map(e => e.stop_id);
 
     return (
       <ScrollView style={styles.container}>
@@ -71,8 +80,23 @@ export default class RouteDetailScreen extends React.Component {
           <CardContent>
             {this.state.stops.map((stop, index) => (
               <View key={index} style={styles.stop}>
+                <Text style={styles.stopInfo}>
+                  {stopsInBus.includes(stop.stop_id) ? (
+                    <Icon.Ionicons
+                      name={
+                        Platform.OS === 'ios'
+                          ? `ios-bus${focused ? '' : '-outline'}`
+                          : 'md-bus'
+                      }
+                      size={20}
+                      style={{ marginBottom: -3 }}
+                      color="#333"
+                    />
+                  ) : (
+                    '○'
+                  )}
+                </Text>
                 <Text style={styles.stopName}>{stop.stop_name}</Text>
-                <Text style={styles.stopInfo}>●</Text>
               </View>
             ))}
           </CardContent>
@@ -89,13 +113,18 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10
   },
   stop: {
+    display: 'flex',
+    flexDirection: 'row',
     paddingVertical: 10,
     paddingHorizontal: 5
   },
+  stopInfo: {
+    color: '#333',
+    fontSize: 15,
+    textAlignVertical: 'center',
+    marginRight: 10
+  },
   stopName: {
     fontSize: 20
-  },
-  stopInfo: {
-    alignSelf: 'flex-end'
   }
 });
